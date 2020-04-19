5adc82c9ffeb40d1e739a6c2a327875b9030a5a2
mriddi <mriddi@bk.ru>
2020-04-19 12:09:47 +0300

    перед Мезенкой

--- server.py
+++ server.py
@@ -4,35 +4,56 @@ ALIVE = 10
 SLEEP = 11
 DEAD = 12
 
-WHITE = 20
-BLACK = 21
-RED = 22
-YELLOW = 23
-
 
 class Game:
 
     def __init__(self):
         self.map = Map()
-        self.players = self.gen_players()
-        self.pirates = pirates
-        self.ships = ships
-        self.coins = coins
-        self.rums = rums
+        self.pirates = self.gen_pirates()
+        self.ships = self.gen_ships()
+        self.coins = self.gen_coins()
+        self.rums = self.gen_rums()
 
-    def gen_players(self):
-        return [Player(0, "Nik", WHITE), Player(1, "Pav", BLACK), Player(2, "Koz", RED), Player(3, "Gay", YELLOW)]
+    @staticmethod
+    def gen_ships():
+        ships = [Ship(0, 45), Ship(1, 45), Ship(2, 45), Ship(3, 45)]
+        return ships
 
-    def gen_pirates(self):
-        players = []
-        for idx in range(4):
-            players.append()
+    @staticmethod
+    def gen_coins():
+        coins = [Coin(36, 47)]
+        for idx in range(36):
+            coins.append(Coin(idx, 46))
+        return coins
 
-    def gen_ships(self):
+    @staticmethod
+    def gen_rums():
+        rums = []
+        for idx in range(9):
+            rums.append(Rum(idx, 52))
+        return rums
 
-    def gen_coins(self):
+    @staticmethod
+    def gen_pirates():
+        pirates = []
+        for idx in range(3):
+            pirates.append(Pirate(idx, 55, ALIVE))
+            pirates.append(Pirate(idx, 56, ALIVE))
+            pirates.append(Pirate(idx, 57, ALIVE))
+            pirates.append(Pirate(idx, 58, ALIVE))
+        pirates.append(Pirate(idx, 49, ALIVE))
+        pirates.append(Pirate(idx, 50, ALIVE))
+        pirates.append(Pirate(idx, 51, ALIVE))
+        return pirates
 
-    def gen_rums(self):
+    def print_info(self):
+        for idx, tile in enumerate(self.map.tiles):
+            txt = str(tile.id) + "/" + str(tile.sprite_id) + "/" + str(tile.rot)
+            if (idx + 1) % 12 == 0:
+                print(txt)
+            else:
+                print(txt, " ", end='')
+
 
 class Map:
 
@@ -50,75 +71,62 @@ class Map:
                 sprite_ids.append(idx + 1)
 
         for idx in range(144):
-            tiles.append(Tile(idx, self.get_sprite_id(sprite_ids), random.choice((0, 90, 180, 270))))
+            tiles.append(Tile(idx, self.get_sprite_id(sprite_ids), random.choice((0, 90, 180, 270)), False))
         return tiles
 
-    def get_sprite_id(self, sprite_ids):
+    @staticmethod
+    def get_sprite_id(sprite_ids):
         num = random.randrange(len(sprite_ids))
         sprite_id = sprite_ids[num]
         del (sprite_ids[num])
         return sprite_id
 
-    def print_info(self):
-        for idx, tile in enumerate(self.tiles):
-            txt = str(tile.id) + "/" + str(tile.sprite_id) + "/" + str(tile.rot)
-            if (idx + 1) % 12 == 0:
-                print(txt)
-            else:
-                print(txt, " ", end='')
-
 
 class Tile:
 
-    def __init__(self, id, sprite_id, rot):
+    def __init__(self, id, sprite_id, rot, fliped):
         self.id = id
         self.sprite_id = sprite_id
         self.rot = rot
+        self.fliped = fliped
 
 
-class Player:
+class MovingObj:
 
-    def __init__(self, id, name, color):
+    def __init__(self, id, sprite_id, x_px, y_px):
         self.id = id
-        self.name = name
-        self.color = color
+        self.sprite_id = sprite_id
+        self.xy_px = [x_px, y_px]
 
 
-class Pirate:
+class Pirate(MovingObj):
 
-    def __init__(self, id, sprite_id, state_id, x_px, y_px):
-        self.id = id
-        self.sprite_id
+    def __init__(self, id, sprite_id, state_id, x_px=0, y_px=0):
+        MovingObj.__init__(self, id, sprite_id, x_px, y_px)
         self.state_id = state_id
-        self.xy_px = [x_px, y_px]
 
 
-class Ship:
+class Ship(MovingObj):
 
-    def __init__(self, id, x_px, y_px):
-        self.id = id
-        self.sprite_id
-        self.xy_px = [x_px, y_px]
+    def __init__(self, id, sprite_id, x_px=0, y_px=0):
+        MovingObj.__init__(self, id, sprite_id, x_px, y_px)
 
 
-class Coin:
+class Coin(MovingObj):
 
-    def __init__(self, id, x_px=20, y_px=20):
-        self.id = id
-        self.sprite_id
-        self.xy_px = [x_px, y_px]
+    def __init__(self, id, sprite_id, x_px=0, y_px=0):
+        MovingObj.__init__(self, id, sprite_id, x_px, y_px)
 
 
 class Rum:
 
-    def __init__(self, id, x_px=10, y_px=10):
-        self.id = id
-        self.xy_px = [x_px, y_px]
+    def __init__(self, id, sprite_id, x_px=0, y_px=0):
+        MovingObj.__init__(self, id, sprite_id, x_px, y_px)
 
 
 def main():
     game = Game()
-    map.print_info()
+    game.print_info()
 
 
 main()
